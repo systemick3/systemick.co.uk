a145052c15a3ce427ed05cc89d2a59d1d92ad47b
Yaniv <git@epale-support.eu>
2016-04-25 09:46:49 +0100

    First changes for adding js and modals

---
+++ themes/systemick/js/systemick.js
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Defines Javascript behaviors for the systemick theme.
+ */
+
+(function ($, Drupal, drupalSettings) {
+
+  Drupal.behaviors.nodeDetailsSummaries = {
+    attach: function (context) {
+
+      $('.view-services .view-content .views-row:nth-child(1) a').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $('.block-views-blockservice-modal-block-1').show();
+        return false;
+      });
+
+      $('.view-services .view-content .views-row:nth-child(2) a').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $('.block-views-blockservice-modal-block-3').show();
+        return false;
+      });
+
+      $('.view-services .view-content .views-row:nth-child(3) a').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $('.block-views-blockservice-modal-block-2').show();
+        return false;
+      });
+
+      $('.block-views .footer .button a').click(function () {
+        $('html, body').animate({ scrollTop: $("#block-contactblock").offset().top }, "slow");
+        $(this).parents('.block-views').hide();
+      });
+
+      $('.menu li:last-child a').click(function () {
+        $('html, body').animate({ scrollTop: $("#block-contactblock").offset().top }, "slow");
+        return false;
+      });
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
