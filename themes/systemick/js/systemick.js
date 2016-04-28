5754a5d05d430878d81af0f1a38e51ed7b3e4f86
Yaniv <git@epale-support.eu>
2016-04-28 17:43:03 +0100

    Fixed js error

--- themes/systemick/js/systemick.js
+++ themes/systemick/js/systemick.js
@@ -39,12 +39,12 @@
       });
 
       // Init Skrollr
-      var s = skrollr.init({
-        render: function(data) {
-          //Debugging - Log the current scroll position.
-          //console.log(data.curTop);
-        }
-      });
+//      var s = skrollr.init({
+//        render: function(data) {
+//          //Debugging - Log the current scroll position.
+//          //console.log(data.curTop);
+//        }
+//      });
 
     }
   };
